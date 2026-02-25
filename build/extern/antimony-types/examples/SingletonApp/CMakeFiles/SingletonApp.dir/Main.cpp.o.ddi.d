b8839d57e00bf88ab4666136074cacece05ce315
guilherme-daros <gh5777.gh@gmail.com>
2026-02-25 12:10:13 -0300

    Add CMakeLists.txt and submodule

---
+++ build/extern/antimony-types/examples/SingletonApp/CMakeFiles/SingletonApp.dir/Main.cpp.o.ddi.d
@@ -0,0 +1,4 @@
+extern/antimony-types/examples/SingletonApp/CMakeFiles/SingletonApp.dir/Main.cpp.o.ddi: \
+ /home/geagaa/workspace/playground/antimony-logger/extern/antimony-types/examples/SingletonApp/Main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/geagaa/workspace/playground/antimony-logger/extern/antimony-types/include/types/Singleton.hpp
